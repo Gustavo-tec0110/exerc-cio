e8ffe816046dae191dd5e2bb32bbb5f6ff199334
Gustavo <gustavo.sl.0110@gmail.com>
2025-12-29 00:00:17 -0300

    Exercício SASS: conversão de CSS com variáveis e módulos

--- java.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('formCadastro');
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  alert('Cadastro realizado com sucesso!');
-  form.reset();
-});
